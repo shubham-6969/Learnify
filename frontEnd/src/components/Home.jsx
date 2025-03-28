7e9d868f0367e1aa3b54a77f420c517604ff5e17
Shubham Patwal <shubhampatwal017@gmail.com>
2025-03-28 10:31:03 +0530

    change in home page

--- frontEnd/src/components/Home.jsx
+++ frontEnd/src/components/Home.jsx
@@ -170,7 +170,9 @@ function Home() {
                       <h2 className="text-xl font-bold text-white">
                         {course.title}
                       </h2>
-                      <Link to={"/courses"} className="mt-4 bg-orange-500 text-white py-2 px-4 rounded-full hover:bg-blue-500 duration-300">
+                      <Link to={`/buy/${course._id}`} 
+                      className="mt-8 bg-orange-500 text-white py-2 px-4 rounded-full hover:bg-blue-500 duration-300"
+                      >
                         Enroll Now
                       </Link>
                     </div>
